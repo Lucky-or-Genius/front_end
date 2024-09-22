36855f2125f800851b6aec85bd11fb1c964bdf11
Harshit Shukla <harshitcr7777@gmail.com>
2024-09-22 16:55:08 +0530

    commiting evidence section

--- src/components/common/accordion.jsx
+++ src/components/common/accordion.jsx
@@ -10,13 +10,19 @@ const Accordion = ({ isOpen, Heading, children }) => {
   const toggleAccordion = () => setOpen((prev) => !prev);
 
   return (
-    <div className="w-full mx-auto bg-white rounded-[12px] border border-smoke_white px-7 py-5">
+    <div className="w-full 2md:w-4/5 mx-auto backdrop-blur-md border border-[#ffffff20] p-4 md:p-6 bg-[#ffffff20] rounded-xl hover:border-primary400 transition-all ease-in-out shadow-black">
       <button
         className="w-full flex items-center justify-between  text-lg font-semibold"
         onClick={toggleAccordion}
       >
-        <span className="text-start">{Heading}</span>
-        <span className={cn("rotate-180 transition-all", { "rotate-0": open })}>
+        <span className="text-start text-white font-raleway text-lg md:text-2xl">
+          {Heading}
+        </span>
+        <span
+          className={cn("rotate-0 transition-all text-white", {
+            "rotate-180": open,
+          })}
+        >
           <FaChevronDown />
         </span>
       </button>
@@ -25,7 +31,7 @@ const Accordion = ({ isOpen, Heading, children }) => {
           "overflow-hidden transition-all duration-500 text-text_secondary font-normal text-start  ",
           {
             "max-h-0 opacity-0": !open,
-            "max-h-screen opacity-100 mt-3": open,
+            " opacity-100 mt-3": open,
           }
         )}
       >
