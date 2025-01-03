5ba90c55bd45b3890097ec7517dce2cb5c55dddd
hs-07 <harshitcr7777@gmail.com>
2025-01-04 03:43:24 +0530

    commiting new leaderboard design, new skeleton & twitter caard fix

--- src/components/newLeaderboard/board.jsx
+++ src/components/newLeaderboard/board.jsx
@@ -1,161 +1,125 @@
 import React from "react";
 
 import { IoMdHeartEmpty, IoMdHeart } from "react-icons/io";
-import { Popover } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 import Skeleton from "./skeleton";
 
 const Board = ({ data, toggleFavourite }) => {
+  const navigate = useNavigate();
+  const Headings = [
+    "Rank",
+    "Name",
+    "Accuracy",
+    "Points",
+    "Bankroll",
+    "Predictions",
+    "Favourite",
+  ];
+
   return (
-    <div className="flex w-full justify-center gap-4 h-full font-poppins md:px-8 px-0">
-      <div className="bg-[#ffffff20] flex justify-start w-full rounded-xl p-2 overflow-y-auto h-fit md:h-4/6 lg:w-5/6 flex-col gap-2 items-center">
-        {data.length > 0 ? (
-          data?.map((item, index) => (
-            <div
-              className="flex items-center gap-4 md:justify-between w-full hover:bg-[#ffffff10] hover:rounded-lg p-2 flex-col md:flex-row"
-              key={index}
+    <div className="flex w-full justify-start lg:justify-center gap-4 h-full md:px-6 overflow-y-hidden">
+      <div className="min-w-[850px] overflow-x-auto w-full md:h-4/6 lg:w-5/6 rounded-2xl bg-[#ffffff10] overflow-y-hidden h-full p-4 relative">
+        {/* Table Headings */}
+        <div className="grid grid-cols-8 border-b pb-4 gap-4 border-gray-500 w-full px-3 sticky top-0 z-30">
+          {Headings.map((item, i) => (
+            <span
+              className={`${
+                item === "Name" ? "col-span-2" : "col-span-1"
+              } w-full text-gray-400 font-raleway font-semibold`}
+              key={i}
             >
-              {" "}
-              <div className="flex gap-4 items-center w-full md:w-fit">
+              {item}
+            </span>
+          ))}
+        </div>
+
+        {/* Table Content */}
+        {data.length > 0 ? (
+          <div className="h-full overflow-y-auto pb-4">
+            {data.map((item, index) => (
+              <Link
+                to={`/dashboard/LeaderBoards/${item.user_id}`}
+                key={index}
+                className={`${
+                  index % 2 === 1 ? "bg-[#ffffff05]" : ""
+                } grid grid-cols-8 w-full py-4 px-2 gap-4 rounded-xl hover:bg-[#ffffff10] transition-all ease-in-out font-poppins cursor-pointer hover:scale-[1.005]`}
+              >
                 <div className="">
                   {item?.rank === 1 ? (
-                    <img alt="rank-1" src="/goldmedal-1.svg" className="w-10" />
+                    <img alt="rank-1" src="/goldmedal-1.svg" className="w-6" />
                   ) : item?.rank === 2 ? (
-                    <img alt="rank-2" src="/goldmedal-2.svg" className="w-10" />
+                    <img alt="rank-2" src="/goldmedal-2.svg" className="w-6" />
                   ) : item?.rank === 3 ? (
-                    <img alt="rank-3" src="/goldmedal-3.svg" className="w-10" />
+                    <img alt="rank-3" src="/goldmedal-3.svg" className="w-6" />
                   ) : (
-                    <div className="bg-[#ffffff40] rounded-full h-8 w-8 flex items-center justify-center text-white">
-                      {" "}
-                      {item?.rank}
-                    </div>
+                    <div className="px-2 text-white">{item?.rank}</div>
                   )}
                 </div>
-                <div className="flex items-center gap-3 w-full">
+                <div className="col-span-2 flex items-center gap-2">
                   <img
                     src={item.image_url}
                     alt="profile"
                     width={40}
                     height={40}
-                    className="rounded-full w-10 h-10 object-cover"
+                    className="rounded-full w-6 h-6 object-cover"
                   />
-                  <div className="w-fit">
-                    <Popover
-                      content={
-                        <>
-                          <div className="flex gap-2 pb-2 font-raleway ">
-                            <img
-                              src={item.image_url}
-                              alt="profile"
-                              width={40}
-                              height={40}
-                              className="rounded-full w-10 h-10 object-cover"
-                            />
-                            <div className="font-[600]">
-                              <span className="text-sm ">
-                                {item.first_name + " " + item.last_name}
-                              </span>
-                              <div className="text-gray-400 text-xs">
-                                <span className="">
-                                  {item.total_predictions}
-                                </span>{" "}
-                                Predictions
-                              </div>
-                            </div>
-                          </div>
-                          <div className="flex w-full justify-between font-[600]">
-                            <div className="flex flex-col w-fit font-raleway items-center">
-                              Points
-                              <span className="text-gray-400 text-xs">
-                                {Math.round(item.total_user_score)}
-                              </span>
-                            </div>
-                            <div className="flex flex-col w-fit font-raleway items-center">
-                              Bankroll
-                              <span className="text-gray-400 text-xs">
-                                {item.bankroll < 0
-                                  ? `-$${Math.abs(item.bankroll)}`
-                                  : `$${item.bankroll}`}
-                              </span>
-                            </div>
-                          </div>
-                        </>
-                      }
-                      title=""
-                      trigger="hover"
+                  <div className="w-fit truncate ">
+                    <Link
+                      to={`/dashboard/LeaderBoards/${item.user_id}`}
+                      className="text-white hover:underline hover:text-primary400 "
                     >
-                      <Link
-                        to={`/dashboard/LeaderBoards/${item.user_id}`}
-                        className="cursor-pointer text-white text-base lg:text-[20px] hover:underline hover:text-primary400"
-                      >
-                        {item.first_name + " " + item.last_name}
-                      </Link>
-                    </Popover>
+                      {item.first_name + " " + item.last_name}
+                    </Link>
                   </div>
                 </div>
-                <div className="md:hidden flex items-center gap-4 text-base">
+                <div className="text-white">
+                  {Math.round(item.prediction_accuracy)} %
+                </div>
+                <div className="text-white">
+                  {Math.round(item.total_user_score)}
+                </div>
+                <div className="text-white">
+                  {item.bankroll < 0
+                    ? `-$${Math.abs(item.bankroll)}`
+                    : `$${item.bankroll}`}
+                </div>
+                <div
+                  className="text-white hover:text-primary400"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    navigate(
+                      `/dashboard/LeaderBoards/${item.user_id}?defaultOpen=predictions`
+                    );
+                  }}
+                >
+                  {item.total_predictions}
+                </div>
+                <div className="">
                   {item.is_favourite ? (
                     <IoMdHeart
-                      onClick={() => toggleFavourite(index, item?.user_id)}
-                      className="cursor-pointer text-error  text-xl"
-                    />
-                  ) : (
-                    <IoMdHeartEmpty
-                      onClick={() => toggleFavourite(index, item?.user_id)}
-                      className="cursor-pointer text-[#ffffff60] text-xl"
-                    />
-                  )}
-                </div>
-              </div>
-              <div className="flex gap-4 w-full md:w-fit">
-                <div className="grid lg:grid-cols-4 grid-cols-2 gap-2 w-full text-[#ffffff60] items-center">
-                  <div className="text-[#ffffff60] text-xs gap-2 px-4 flex-col flex items-center  py-2 rounded-lg ">
-                    Accuracy
-                    <span className="text-white font-semibold text-lg">
-                      {Math.round(item.prediction_accuracy)} %
-                    </span>
-                  </div>
-                  <div className="text-[#ffffff60] text-xs gap-2 px-4 border border-[#ffffff60] flex-col flex items-center  py-2 rounded-lg ">
-                    Points
-                    <span className="text-white">
-                      {Math.round(item.total_user_score)}
-                    </span>
-                  </div>
-                  <div className="text-[#ffffff60] text-xs gap-2 px-4 border border-[#ffffff60] flex-col flex items-center  py-2 rounded-lg ">
-                    Bankroll
-                    <span className="text-white">
-                      {item.bankroll < 0
-                        ? `-$${Math.abs(item.bankroll)}`
-                        : `$${item.bankroll}`}
-                    </span>
-                  </div>
-                  <Link
-                    className="text-[#ffffff60] text-xs py-2 gap-2 px-4 rounded-lg  hover:bg-[#ffffff40] border border-[#ffffff60] flex-col flex items-center"
-                    to={`/dashboard/LeaderBoards/${item.user_id}?defaultOpen=predictions`}
-                  >
-                    {" "}
-                    Predictions
-                    <span className="text-primary400">
-                      {item.total_predictions}
-                    </span>
-                  </Link>
-                </div>
-                <div className="hidden md:flex items-center gap-4 text-base">
-                  {item.is_favourite ? (
-                    <IoMdHeart
-                      onClick={() => toggleFavourite(index, item?.user_id)}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        toggleFavourite(index, item?.user_id);
+                      }}
                       className="cursor-pointer text-error  text-xl active:scale-95 transition-all hover:scale-105"
                     />
                   ) : (
                     <IoMdHeartEmpty
-                      onClick={() => toggleFavourite(index, item?.user_id)}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        toggleFavourite(index, item?.user_id);
+                      }}
                       className="cursor-pointer text-[#ffffff60] text-xl active:scale-95 transition-all hover:scale-105"
                     />
                   )}
                 </div>
-              </div>
-            </div>
-          ))
+              </Link>
+            ))}
+          </div>
         ) : (
           <Skeleton />
         )}
--- src/components/newLeaderboard/skeleton.jsx
+++ src/components/newLeaderboard/skeleton.jsx
@@ -3,30 +3,65 @@ import React from "react";
 const skeleton = () => {
   return (
     <div className="flex flex-col gap-2 w-full">
-      <div className="h-20 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg flex items-center px-4 gap-4">
+      <div className="h-14 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg grid grid-cols-8 items-center px-4 gap-4">
+        <div className="w-4 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="flex gap-2 items-center col-span-2">
+          <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+          <div className="w-28 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        </div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
         <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-10 h-10 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-28 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
       </div>
-      <div className="h-20 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg flex items-center px-4 gap-4">
+      <div className="h-14 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg grid grid-cols-8 items-center px-4 gap-4">
+        <div className="w-4 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="flex gap-2 items-center col-span-2">
+          <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+          <div className="w-28 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        </div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
         <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-10 h-10 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-28 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
       </div>
-      <div className="h-20 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg flex items-center px-4 gap-4">
+      <div className="h-14 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg grid grid-cols-8 items-center px-4 gap-4">
+        <div className="w-4 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="flex gap-2 items-center col-span-2">
+          <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+          <div className="w-28 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        </div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
         <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-10 h-10 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-28 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
       </div>
-      <div className="h-20 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg flex items-center px-4 gap-4">
+      <div className="h-14 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg grid grid-cols-8 items-center px-4 gap-4">
+        <div className="w-4 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="flex gap-2 items-center col-span-2">
+          <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+          <div className="w-28 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        </div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
         <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-10 h-10 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-28 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
       </div>
-      <div className="h-20 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg flex items-center px-4 gap-4">
+      <div className="h-14 w-full bg-[#ffffff20] animate-pulse transition-all rounded-lg grid grid-cols-8 items-center px-4 gap-4">
+        <div className="w-4 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="flex gap-2 items-center col-span-2">
+          <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+          <div className="w-28 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        </div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
+        <div className="w-12 h-4 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
         <div className="w-6 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-10 h-10 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
-        <div className="w-28 h-6 rounded-full bg-[#ffffff30] animate-pulse transition-all"></div>
       </div>
     </div>
   );
