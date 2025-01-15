b3c893cea753869f7a58d43fe62b5eb7daf204b2
hs-07 <harshitcr7777@gmail.com>
2025-01-15 19:48:10 +0530

    fixed infinite scroll on feed

--- src/services/Feed.service.js
+++ src/services/Feed.service.js
@@ -3,10 +3,10 @@ import Axios from "../utils/feed-axios";
 export const getFeedDetails = async (accountId, number) => {
   if (accountId)
     return await Axios.get(
-      `predictions/?offset=${number}&limit=100&account_id=${accountId}`
+      `predictions/?snapshot_offset=${number}&account_id=${accountId}`
     );
   else
     return await Axios.get(
-      `predictions/?offset=${number}&limit=100&account_id=003d45e5-b3a2-40c0-8e76-59ef89f6a519`
+      `predictions/?snapshot_offset=${number}&account_id=003d45e5-b3a2-40c0-8e76-59ef89f6a519`
     );
 };
