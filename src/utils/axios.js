c2ad4970d27fbc30e04b3eae77fe368377791615
GZ-01 <173709288+GZ-01@users.noreply.github.com>
2025-02-12 12:00:37 -0600

    fixed accountId logic

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -8,8 +8,7 @@ const Axios = axios.create({
 // Get account ID from local storage or use demo account
 const getAccountId = () => {
   try {
-    const storedId = localStorage.getItem('accountId');
-    return storedId ? JSON.parse(storedId) : '003d45e5-b3a2-40c0-8e76-59ef89f6a519';
+    return localStorage.getItem('accountId') || '003d45e5-b3a2-40c0-8e76-59ef89f6a519';
   } catch {
     return '003d45e5-b3a2-40c0-8e76-59ef89f6a519';
   }
